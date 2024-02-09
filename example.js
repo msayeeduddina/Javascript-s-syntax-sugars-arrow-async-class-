107adcfa95f20afb517a6789350ef1d996c68b2b
SAYEED <90544352+msayeeduddina@users.noreply.github.com>
2024-02-09 15:23:11 +0530

    Update example.js

--- example.js
+++ example.js
@@ -3,6 +3,7 @@ const myobj = {
     name: "fred"
 };
 
+// Log a separator line and the content of myobj
 console.log("--------");
 console.log(myobj);
 
@@ -22,6 +23,7 @@ myobj.method1 = function() {
     a();
     b();
 
+    // Log a separator line
     console.log("--------");
 
     // Define another object called otherobj with a property 'name'
@@ -41,3 +43,50 @@ myobj.method1 = function() {
 
 // Call method1 of myobj to demonstrate its behavior
 myobj.method1();
+
+// Log another separator line
+console.log("--------||--------");
+
+// Create a promise resolved with a value of 1 and log it
+const promiseFromValue = Promise.resolve(1);
+console.log(promiseFromValue);
+
+// Create a promise resolved with a value of 2 using a constructor function and log it
+const promiseFromFunction = new Promise(
+    function(resolve,reject){
+        resolve(2);
+    }
+);
+console.log(promiseFromFunction);
+
+// Log a separator line
+console.log("--------");
+
+// Create two promises that resolve with random delays and log their values
+const promise1 = new Promise(
+    function(resolve,reject){
+        setTimeout(()=>{
+            resolve(1);
+        }, Math.random()*100);
+    }
+);
+
+const promise2 = new Promise(
+    function(resolve,reject){
+        setTimeout(()=>{
+            resolve(2);
+        }, Math.random()*100);
+    }
+);
+
+promise1.then(value=>console.log("promise1",value));
+promise2.then(value =>console.log("promise2",value));
+
+// Define an asynchronous function called main to demonstrate async/await
+const main = async () => {
+    console.log("promise1", await promise1); // Wait for promise1 to resolve and log its value
+    console.log("promise2", await promise2); // Wait for promise2 to resolve and log its value
+};
+main(); // Call the main function
+
+console.log("--main()--"); // Log a separator line after calling main()
